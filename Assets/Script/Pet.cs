09914f09c2df1fb6f9332046eed4ec3d3213041e
JohanSvensson <js@resolutiongames.com>
2017-01-19 09:37:54 +0100

    A box now moved to where you click.

---
+++ Assets/Script/Pet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using RGCommon;
+
+public class Pet : MonoBehaviour {
+
+    private NavMeshAgent navAgent;
+
+	void Awake() {
+        navAgent = Find.ComponentOnGameObject<NavMeshAgent>(this);
+        PointerInteractor.MoveSignal.AddListener(OnMoveSignal);
+	}
+    
+	void Update () {
+
+	}
+
+    private void OnMoveSignal(Vector3 target) {
+        navAgent.SetDestination(target);
+    }
+
+}
--- Assets/Script/PointerInteractor.cs
+++ Assets/Script/PointerInteractor.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using CakewalkIoC.Signal;
 
 public class PointerInteractor : MonoBehaviour {
+
+    public static Signal<Vector3> MoveSignal = new Signal<Vector3>();
 
     public GameObject reticlePrefab;
 
@@ -10,6 +14,7 @@ public class PointerInteractor : MonoBehaviour {
     private Vector3 outOfBoundsPosition = new Vector3(-999999, -999999, -99999); // hopefully not visible.
     private int interactionLayer = 1 << 8;
 
+    private bool hasValidMoveTarget = false;
 
     void Awake() {
         reticle = (GameObject)Instantiate(reticlePrefab, transform.position, Quaternion.identity);
@@ -20,11 +25,19 @@ public class PointerInteractor : MonoBehaviour {
         Ray ray = new Ray(transform.position, transform.forward);
         
         if(Physics.Raycast(ray, out hit, Mathf.Infinity, interactionLayer)) {
-            reticle.transform.position = hit.point;
+            reticle.transform.position = hit.point + new Vector3(0,0.05f,0);
+            reticle.transform.forward = -Vector3.up;
+            hasValidMoveTarget = true;
+
+            if(RGInput.Instance.ButtonWasPressed(RGInput.Button.Click)) {
+                MoveSignal.Dispatch(hit.point);
+            }
         }
         else {
             reticle.transform.position = outOfBoundsPosition;
+            hasValidMoveTarget = false;
         }
         
     }
+    
 }
